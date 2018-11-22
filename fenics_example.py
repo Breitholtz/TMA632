156c4e402e4d38e9e51d9b7a3fdc231bd0c79cd3
Breitholtz <cadbreitholtz@gmail.com>
2018-11-22 14:24:55 +0100

    periodic on 'rectangular' mesh

--- fenics_example.py
+++ fenics_example.py
@@ -1,17 +1,27 @@
 from fenics import *
 
-# Create mesh and define function space
-mesh = UnitSquareMesh(8, 8)
-V = FunctionSpace(mesh, 'P', 1)
 
 # Define boundary condition
 u_D = Expression("1 + x[0]*x[0] + 2*x[1]*x[1]", degree=2)
 
+
 def boundary(x, on_boundary):
 	return on_boundary
 
-bc = DirichletBC(V, u_D, boundary)
-
+class PeriodicBoundary(SubDomain):
+	# left boundary
+	def inside(self,x,on_boundary):
+		return bool(x[0] < DOLFIN_EPS and x[0]> -DOLFIN_EPS and on_boundary)
+	# right boundary
+	def map(self,x,y):
+		y[0]=x[0]-2*DOLFIN_PI
+		y[1]=x[1]
+pbc = PeriodicBoundary()
+# mesh and space
+L=DOLFIN_PI*2
+SIZE=10
+mesh = RectangleMesh(Point(0.0,0.0),Point(L, L),SIZE,SIZE,"right")
+V=FunctionSpace(mesh, "CG",1,constrained_domain=pbc)
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
@@ -21,7 +31,7 @@ L = f*v*dx
 
 # Compute solution
 u = Function(V)
-solve(a == L, u, bc)
+solve(a == L, u)
 
 # Plot solution and mesh
 plot(u)
