7281fd1c8c392057752cc6c00ceaefe956992225
Breitholtz <cadbreitholtz@gmail.com>
2018-12-03 13:32:27 +0100

    working?

--- Exempel/2Dtest.py
+++ Exempel/2Dtest.py
@@ -30,24 +30,24 @@ V = FunctionSpace(mesh, 'P', 1,constrained_domain=PeriodicBoundary())
 
 
 
-# Define dirichlet boundary condition
-u_D = Expression('1 + x[0]*x[0] + alpha*x[1]*x[1] + beta*t',
-                 degree=2, alpha=alpha, beta=beta, t=0)
+# Define initial boundary condition
+u_0 = Expression('exp(t)*sin(x[0])*cos(x[1])',
+                 degree=2, t=0)
 
 def boundary(x, on_boundary):
     return on_boundary
 
-bc = DirichletBC(V, u_D, boundary)
+bc = DirichletBC(V, u_0, boundary)
 
-# Define initial value
-u_n = interpolate(Constant(1), V)
-#u_n = project(u_D, V)
+# Define initial value as interpolation of DBC
+u_n = interpolate(u_0, V)
+
 
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
 f = Constant(0)
-B= Expression("x[0]","0",degree=3) # velocity field for convection
+B= Constant((1,1)) # velocity field
 
 F = dt*dot(B,grad(u))*v*dx + dt*dot(grad(u), grad(v))*dx - (u_n + dt*f)*v*dx 
 a, L = lhs(F), rhs(F)
@@ -59,12 +59,10 @@ for n in range(num_steps):
 
     # Update current time
     t += dt
-    u_D.t = t
+    u_0.t = t
 
     # Compute solution
-    solve(a == L, u, bc)
-
-  
+    solve(a == L, u, bc) 
 
     # Update previous solution
     u_n.assign(u)
