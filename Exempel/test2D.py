3f11f6d1a7e1b6006010335f52a1477236433a3c
Blajpaj <43758965+Blajpaj@users.noreply.github.com>
2018-12-06 16:17:02 +0100

    Updated 2D case

--- Exempel/test2D.py
+++ Exempel/test2D.py
@@ -9,8 +9,8 @@ from dolfin import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-T = 5            # final time
-dt = 0.05 # time step size
+T = 5     # final time
+dt = 0.1 # time step size
 num_steps = int(T/dt) # number of time steps
 
 # Sub domain for Periodic boundary condition
@@ -19,9 +19,8 @@ class PeriodicBoundary(SubDomain):
     # Left boundary is "target domain" G
     def inside(self, x, on_boundary):
         # return True if on left or bottom boundary AND NOT on one of the two corners (0, 2*PI) and (2*PI, 0)
-        return bool((near(x[0], 0) or near(x[1], 0)) and 
-                (not ((near(x[0], 0) and near(x[1], 2*DOLFIN_PI)) or 
-                        (near(x[0], 2*DOLFIN_PI) and near(x[1], 0)))) and on_boundary)
+        return bool((near(x[0], 0) or near(x[1], 0)) and on_boundary)
+		# and (not ((near(x[0], 0) and near(x[1], 2*DOLFIN_PI)) or (near(x[0], 2*DOLFIN_PI) and near(x[1], 0)))) --- Is this necessary?
 
     def map(self, x, y):
         if near(x[0], 2*DOLFIN_PI) and near(x[1], 2*DOLFIN_PI):
@@ -44,10 +43,14 @@ u = TrialFunction(V)
 v = TestFunction(V)
 
 u_0 = Expression('exp(-t)*(sin(x[0]) + cos(x[1]))', degree=2, t=0)
-f = Expression('b_1*exp(-t)*cos(x[0]) - b_2*exp(-t)*sin(x[1])', degree=2, t=0, b_1=1.0, b_2=1.0)
+f = Expression('b_1*exp(-t)*cos(x[0]) - b_2*exp(-t)*sin(x[1])', degree=2, t=0, b_1=Constant(1), b_2=Constant(1))
 u_n = project(u_0, V)
 
-a = u*v*dx + dt*dot(grad(u), grad(v))*dx + dt*grad(u)[0]*v*dx
+#f.b_1 = Expression('x[0]',degree=2)
+#f.b_2 = Expression('x[1]',degree=2)
+b = Constant((1,1))
+
+a = u*v*dx + dt*dot(grad(u), grad(v))*dx + dt*dot(b,grad(u))*v*dx
 L = (u_n + dt*f)*v*dx
 
 # Create VTK file for saving solution
