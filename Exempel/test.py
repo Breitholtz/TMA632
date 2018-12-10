612d789b69c0bb9dfd2137c0a98612051d0218ec
Blajpaj <43758965+Blajpaj@users.noreply.github.com>
2018-12-10 13:35:36 +0100

    Updated previous examples

--- Exempel/test.py
+++ Exempel/test.py
@@ -3,29 +3,30 @@
 #     u_t - u_xx + u_x = f(x, t)
 #
 # on the interval [0,2*PI] with periodic boundary conditions.
-# Initial value is u_0 = sin(x) and f = e^(-t)*cos(x).
+# Initial value is u_0 = sin(x) with source term f = b(x)*e^(-t)*cos(x).
 
 from dolfin import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-T = 2.0            # final time
-num_steps = 10     # number of time steps
-dt = T / num_steps # time step size
+T = 5 # final time
+dt = 0.1 # time step size
+num_steps = int(T/dt) # number of time steps
 
 # Sub domain for Periodic boundary condition
 class PeriodicBoundary(SubDomain):
 
     # Left boundary is "target domain" G
     def inside(self, x, on_boundary):
-        return bool(x[0] < DOLFIN_EPS and x[0] > -DOLFIN_EPS and on_boundary)
+        return bool(near(x[0],0) and on_boundary)
 
     # Map right boundary (H) to left boundary (G)
     def map(self, x, y):
         y[0] = x[0] - 2*DOLFIN_PI
 
 # Create mesh and finite element
-mesh = IntervalMesh(64, 0, 2*DOLFIN_PI)
+nx = 64
+mesh = IntervalMesh(nx, 0, 2*DOLFIN_PI)
 V = FunctionSpace(mesh, "CG", 1, constrained_domain=PeriodicBoundary())
 
 # Define variational problem
@@ -34,16 +35,18 @@ v = TestFunction(V)
 
 u_0 = Expression('exp(-t)*sin(x[0])', degree=2, t=0)
 f = Expression('exp(-t)*cos(x[0])', degree=2, t=0)
-u_n = interpolate(u_0, V)
+u_n = project(u_0, V)
 
-a = u*v*dx + dt*dot(grad(u), grad(v))*dx + dt*u.dx(0)*v*dx
-L = (u_n + dt*f)*v*dx
+b = Expression('x[0]*x[0]', degree=2)
+
+a = u*v*dx + dt*dot(grad(u), grad(v))*dx + dt*b*grad(u)[0]*v*dx
+L = (u_n + dt*dot(b,f))*v*dx
 
 # Create VTK file for saving solution
-vtkfile = File('test/solution.pvd')
+vtkfile = File('test/solution1D.pvd')
 
 u = Function(V)
-t = 0
+t = 0.0
 for n in range(num_steps):
 
     # Update current time
@@ -62,7 +65,7 @@ for n in range(num_steps):
     plt.plot(x[i],ua[i]);
 
     # Compute error at vertices
-    u_e = interpolate(u_0, V)
+    u_e = project(u_0, V)
     error = np.abs(u_e.vector().array() - u.vector().array()).max()
     print('t = %.2f: error = %.3g' % (t, error))
 
--- Exempel/test2D.py
+++ Exempel/test2D.py
@@ -3,25 +3,25 @@
 #     u_t - Laplace(u) + dot(b,grad(u)) = f(x, y, t)
 #
 # on the square [0,2*PI]^2 with periodic boundary conditions.
-# Initial value is u_0 = sin(x) + cos(y) and f = b_1(x, y)*e^(-t)*cos(x) - b_2(x, y)*e^(-t)*sin(y).
+# Initial value is u_0 = sin(x) + cos(y) with source term 
+# f = b_1(x, y)*e^(-t)*cos(x) - b_2(x, y)*e^(-t)*sin(y).
 
 from dolfin import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-T = 5     # final time
+T = 5 # final time
 dt = 0.1 # time step size
 num_steps = int(T/dt) # number of time steps
 
 # Sub domain for Periodic boundary condition
 class PeriodicBoundary(SubDomain):
 
-    # Left boundary is "target domain" G
+    # Left and bottom boundary is "target domain" G
     def inside(self, x, on_boundary):
-        # return True if on left or bottom boundary AND NOT on one of the two corners (0, 2*PI) and (2*PI, 0)
         return bool((near(x[0], 0) or near(x[1], 0)) and on_boundary)
-		# and (not ((near(x[0], 0) and near(x[1], 2*DOLFIN_PI)) or (near(x[0], 2*DOLFIN_PI) and near(x[1], 0)))) --- Is this necessary?
 
+    # Map right boundary (H) to left boundary (G)
     def map(self, x, y):
         if near(x[0], 2*DOLFIN_PI) and near(x[1], 2*DOLFIN_PI):
             y[0] = x[0] - 2*DOLFIN_PI
@@ -29,32 +29,30 @@ class PeriodicBoundary(SubDomain):
         elif near(x[0], 2*DOLFIN_PI):
             y[0] = x[0] - 2*DOLFIN_PI
             y[1] = x[1]
-        else:   # near(x[1], 1)
+        else:   # near(x[1], 2*DOLFIN_PI)
             y[0] = x[0]
             y[1] = x[1] - 2*DOLFIN_PI
 
 # Create mesh and finite element
 nx = ny = 64
 mesh = RectangleMesh(Point(0, 0), Point(2*DOLFIN_PI, 2*DOLFIN_PI), nx, ny)
-V = FunctionSpace(mesh, "CG", 1, constrained_domain=PeriodicBoundary())
+V = FunctionSpace(mesh, "P", 1, constrained_domain=PeriodicBoundary())
 
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
 
 u_0 = Expression('exp(-t)*(sin(x[0]) + cos(x[1]))', degree=2, t=0)
-f = Expression('b_1*exp(-t)*cos(x[0]) - b_2*exp(-t)*sin(x[1])', degree=2, t=0, b_1=Constant(1), b_2=Constant(1))
+f = Expression(('exp(-t)*cos(x[0])','-exp(-t)*sin(x[1])'), degree=2, t=0)
 u_n = project(u_0, V)
 
-#f.b_1 = Expression('x[0]',degree=2)
-#f.b_2 = Expression('x[1]',degree=2)
-b = Constant((1,1))
+b = Expression(('x[0]*x[0]*sin(x[1])','x[1]*x[1]*cos(x[0])'), degree=2)
 
 a = u*v*dx + dt*dot(grad(u), grad(v))*dx + dt*dot(b,grad(u))*v*dx
-L = (u_n + dt*f)*v*dx
+L = (u_n + dt*dot(b,f))*v*dx
 
 # Create VTK file for saving solution
-vtkfile = File('test/solution.pvd')
+vtkfile = File('test/solution2D.pvd')
 
 u = Function(V)
 t = 0.0
